400ff62509d8998401ad6e13567e933de7597dc9
Jacob Deffendall <jddeffendall@bsu.edu>
2020-03-31 11:30:41 -0400

    Initial project files

--- src/main/java/utils/HTTPUtils.java
+++ src/main/java/utils/HTTPUtils.java
@@ -1,4 +1,38 @@
 package utils;
 
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.javanet.NetHttpTransport;
+
+import java.io.IOException;
+
 public class HTTPUtils {
+
+    HttpRequestFactory requestFactory;
+    String baseURL = "https://todoserver222.herokuapp.com/";
+    String todosURL = baseURL + "todos/";
+
+    public HTTPUtils() {
+        requestFactory = new NetHttpTransport().createRequestFactory();
+    }
+
+    public String getTodoItemJsonString(int id) throws IOException {
+        HttpRequest getRequest = requestFactory.buildGetRequest(
+                new GenericUrl(todosURL + id));
+        String rawResponse = getRequest.execute().parseAsString();
+        return rawResponse;
+    }
+
+    public boolean deleteTodoItem(int id) throws IOException {
+        try {
+            HttpRequest deleteRequest = requestFactory.buildDeleteRequest(
+                    new GenericUrl(todosURL + id));
+            String rawResponse = deleteRequest.execute().parseAsString();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
+    }
+
 }
