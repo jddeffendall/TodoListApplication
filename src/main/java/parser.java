a028cdd00f406d0adc8453a878939eb69f83537c
hadenfoster <hgfoster@bsu.edu>
2020-03-31 11:09:42 -0400

    check this parser

---
+++ src/main/java/parser.java
@@ -0,0 +1,23 @@
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class parser {
+    public JsonObject parseData(InputStream input) {
+        Reader reader = new InputStreamReader(input);
+        JsonElement e = JsonParser.parseReader(reader);
+        JsonObject object = e.getAsJsonObject();
+        if (!object.has("complete!")) {
+            JsonObject query = object.getAsJsonObject("query");
+            return query;
+
+        } else {
+            return null;
+        }
+
+    }
+}
--- src/main/java/utils/HTTPutils.java
+++ src/main/java/utils/HTTPutils.java
@@ -1,4 +1,8 @@
 package utils;
 
 public class HTTPutils {
+
+
+
+
 }
