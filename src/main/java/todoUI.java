6edeb4f0a87ca0a9e3fbc2701282670c6e50959f
Ross Effinger <reffinger17@gmail.com>
2020-03-31 10:34:12 -0500

    started UI

--- src/main/java/todoUI.java
+++ src/main/java/todoUI.java
@@ -1,2 +1,36 @@
-public class todoUI {
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class todoUI extends JFrame implements ActionListener {
+
+    public todoUI(){
+        super("Todo Application");
+        UIManager.put("Label.font", new FontUIResource(new Font("Dialog", Font.PLAIN, 20)));
+        UIManager.put("Button.font", new FontUIResource(new Font("Dialog", Font.PLAIN, 20)));
+
+        JPanel panel = new JPanel();
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        panel.setLayout(gridBagLayout);
+        setContentPane(panel);
+
+
+        setPreferredSize(new Dimension(500,500));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setVisible(true);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new todoUI();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
