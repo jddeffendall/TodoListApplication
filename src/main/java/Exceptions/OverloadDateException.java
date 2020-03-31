31d3838f71edf4260c3b4cd8583a0e28d5efb768
hadenfoster <hgfoster@bsu.edu>
2020-03-31 11:30:36 -0400

    Exceptions added

---
+++ src/main/java/Exceptions/OverloadDateException.java
@@ -0,0 +1,4 @@
+package Exceptions;
+
+public class OverloadDateException extends Throwable {
+}
--- src/main/java/parser.java
+++ src/main/java/parser.java
@@ -7,10 +7,14 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class parser {
+
+
     public JsonObject parseData(InputStream input) {
         Reader reader = new InputStreamReader(input);
         JsonElement e = JsonParser.parseReader(reader);
         JsonObject object = e.getAsJsonObject();
+
+
         if (!object.has("complete!")) {
             JsonObject query = object.getAsJsonObject("query");
             return query;
@@ -20,4 +24,6 @@ public class parser {
         }
 
     }
+
+
 }
