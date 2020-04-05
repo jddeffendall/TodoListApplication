d6767bf61ca7f5febf763fe7a671cb65b16a33bf
Jacob Deffendall <jddeffendall@bsu.edu>
2020-04-05 14:29:03 -0400

    Updated parser

--- src/main/java/parser.java
+++ src/main/java/parser.java
@@ -1,10 +1,18 @@
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.sun.tools.javac.comp.Todo;
+import domain.TodoItem;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class parser {
 
@@ -23,6 +31,13 @@ public class parser {
             return null;
         }
 
+    }
+
+    public List<TodoItem> JsonStringToObjects(String userJson) {
+        Gson gson = new Gson();
+        Type todoListType = new TypeToken<ArrayList<TodoItem>>(){}.getType();
+        List<TodoItem> todoList = gson.fromJson(userJson, todoListType);
+        return todoList;
     }
 
 
