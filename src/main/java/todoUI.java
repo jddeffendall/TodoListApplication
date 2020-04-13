8b0c63fd364aad7d658a92b811e2c710900a3bb2
hadenfoster <hgfoster@bsu.edu>
2020-04-13 12:13:47 -0400

    buttons fix , new UI layout done for now, ready for more buttons

--- src/main/java/todoUI.java
+++ src/main/java/todoUI.java
@@ -50,7 +50,7 @@ public class todoUI extends JFrame{
 */
 
         JButton refresh = new JButton("Refresh");
-        var refreshConstraints = new GridBagConstraints(4, 2, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        var refreshConstraints = new GridBagConstraints(6, 2, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         panel.add(refresh, refreshConstraints);
         refresh.addActionListener(e->{
             try {
@@ -60,28 +60,43 @@ public class todoUI extends JFrame{
             }
         });
 
-        JLabel titlee = new JLabel("Enter Title of Todo Item:");
-        var titleeConstraints = new GridBagConstraints(0, 2, 1, 1, 1, 1,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+
+
+
+
+        JLabel titlee = new JLabel("Enter Title of Item:");
+        var titleeConstraints = new GridBagConstraints(0, 3, 1, 1, 1, 1,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         panel.add(titlee, titleeConstraints);
 
-
-
-
-
-        JTextField titleInput = new JTextField("##############");
-        var titleInputConstraints = new GridBagConstraints(0,1,1,1,1,1,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
+        JTextField titleInput = new JTextField("");
+        var titleInputConstraints = new GridBagConstraints(1,3,1,1,1,1,GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
         panel.add(titleInput, titleInputConstraints);
 
-        JTextField dueDateInput = new JTextField("Enter Due Date For Item:");
-        var dueDateInputConstraints = new GridBagConstraints(1,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0, 0);
+
+
+        JLabel dueDateLabel = new JLabel("Enter Due Date For Item:");
+        var dueDateLabelConstraints = new GridBagConstraints(2,3,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0, 0);
+        panel.add(dueDateLabel, dueDateLabelConstraints);
+
+        JTextField dueDateInput = new JTextField("");
+        var dueDateInputConstraints = new GridBagConstraints(3,3,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0, 0);
         panel.add(dueDateInput, dueDateInputConstraints);
 
-        JTextField deleteItemByIdinput = new JTextField("Enter Id of Todo Item to Delete:");
-        var deleteItemByIdConstraints = new GridBagConstraints(2,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
+
+
+        JLabel deleteItemLabel = new JLabel("Enter Id of Item to Delete:");
+        var deleteItemLabelConstraints = new GridBagConstraints(0,4,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
+        panel.add(deleteItemLabel, deleteItemLabelConstraints);
+
+        JTextField deleteItemByIdinput = new JTextField("");
+        var deleteItemByIdConstraints = new GridBagConstraints(2,4,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
         panel.add(deleteItemByIdinput, deleteItemByIdConstraints);
 
+
+
+
         JButton AddEvent = new JButton("Add To Schedule");
-        var AddEventConstraints = new GridBagConstraints(4, 3, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        var AddEventConstraints = new GridBagConstraints(6, 3, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         AddEvent.addActionListener(e ->{
             String title = titleInput.getText();
             String due = dueDateInput.getText();
@@ -94,7 +109,7 @@ public class todoUI extends JFrame{
         panel.add(AddEvent, AddEventConstraints);
 
         JButton Cancel = new JButton("Cancel Event");
-        var CancelConstraints = new GridBagConstraints(4, 4, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        var CancelConstraints = new GridBagConstraints(6, 4, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         panel.add(Cancel, CancelConstraints);
         Cancel.addActionListener(e->{
             String stringId = deleteItemByIdinput.getText();
@@ -106,12 +121,26 @@ public class todoUI extends JFrame{
             }
         });
 
-        JTextField completeEventById = new JTextField("Enter ID of item to set as complete");
+
+
+
+
+        JLabel completeEventLabel = new JLabel("Enter ID of item to set as complete");
+        var completeEventLabelConstraints = new GridBagConstraints(2,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
+        panel.add(completeEventLabel, completeEventLabelConstraints);
+
+        JTextField completeEventById = new JTextField("");
         var completeEventByIdConstraints = new GridBagConstraints(3,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
         panel.add(completeEventById, completeEventByIdConstraints);
 
+
+
+
+
+
+
         JButton completeEvent = new JButton("Complete Event");
-        var completeEventConstraints = new GridBagConstraints(4,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
+        var completeEventConstraints = new GridBagConstraints(6,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
         panel.add(completeEvent, completeEventConstraints);
         completeEvent.addActionListener(e ->{
             String idToComplete = completeEventById.getText();
@@ -125,7 +154,7 @@ public class todoUI extends JFrame{
             }
         });
 
-        setPreferredSize(new Dimension(800,800));
+        setPreferredSize(new Dimension(1200,1200));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
         setVisible(true);
