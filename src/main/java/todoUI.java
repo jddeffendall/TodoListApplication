95d3c22334791310ec69178a07374b0237043337
Ross Effinger <reffinger17@gmail.com>
2020-04-15 00:48:57 -0500

    UI improvements and fixed the dang complete button that I messed up

--- src/main/java/todoUI.java
+++ src/main/java/todoUI.java
@@ -26,7 +26,7 @@ public class todoUI extends JFrame implements ActionListener {
         //panel.setLayout(gridBagLayout);
         setContentPane(panel);
         panel.setLayout(null);
-        panel.setPreferredSize(new Dimension(1050,1050));
+        panel.setPreferredSize(new Dimension(1050, 1050));
 
 
         HTTPUtils httpUtils = new HTTPUtils();
@@ -40,7 +40,7 @@ public class todoUI extends JFrame implements ActionListener {
         String[] columnNames = {"Created", "Description", "Due", "Completed", "ID"};
 
         JTable items = new JTable(data, columnNames);
-        items.setBounds(0,0, 600, 800);
+        items.setBounds(0, 0, 600, 800);
         //var itemsConstraints = new GridBagConstraints(0, 0, 4, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         panel.add(items);
 
@@ -56,26 +56,28 @@ public class todoUI extends JFrame implements ActionListener {
         JLabel titlee = new JLabel("Enter Title of Item:");
         titlee.setFont(new Font("Serif", Font.PLAIN, 18));
         //titlee.setVerticalAlignment(SwingConstants.TOP);
-        titlee.setBounds(600,0, 200, 50);
+        titlee.setBounds(600, 0, 200, 50);
         panel.add(titlee);
 
         JTextField titleInput = new JTextField("");
-        titleInput.setBounds(600,50,200,50);
+        titleInput.setBounds(600, 50, 200, 50);
+        titleInput.setBackground(Color.lightGray);
         panel.add(titleInput);
 
         JLabel dueDateLabel = new JLabel("Enter Due Date For Item:");
-        dueDateLabel.setBounds(600,100,200,50);
+        dueDateLabel.setBounds(600, 100, 200, 50);
         dueDateLabel.setFont(new Font("Serif", Font.PLAIN, 18));
         panel.add(dueDateLabel);
 
         JTextField dueDateInput = new JTextField("");
-        dueDateInput.setBounds(800,100,250,50);
+        dueDateInput.setBounds(800, 100, 250, 50);
+        dueDateInput.setBackground(Color.lightGray);
         panel.add(dueDateInput);
 
         JButton AddEvent = new JButton("Add To Schedule");
-        AddEvent.setPreferredSize(new Dimension(250,100));
+        AddEvent.setPreferredSize(new Dimension(250, 100));
         Dimension size = AddEvent.getPreferredSize();
-        AddEvent.setBounds(800,0, size.width, size.height);
+        AddEvent.setBounds(800, 0, size.width, size.height);
         AddEvent.addActionListener(e -> {
             String title = titleInput.getText();
             String due = dueDateInput.getText();
@@ -88,25 +90,26 @@ public class todoUI extends JFrame implements ActionListener {
 
         panel.add(AddEvent);
 
-        JLabel completeEventLabel = new JLabel("<HTML>Enter ID of item to set as complete:<HTML>");
+        JLabel completeEventLabel = new JLabel("<HTML>Enter ID of item to set as complete<HTML>");
+        //var completeEventLabelConstraints = new GridBagConstraints(2,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
+        completeEventLabel.setBounds(600,150,200,50);
         completeEventLabel.setFont(new Font("Serif", Font.PLAIN, 18));
-        completeEventLabel.setBounds(600,100,200,150);
-        //var completeEventLabelConstraints = new GridBagConstraints(2, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         panel.add(completeEventLabel);
 
         JTextField completeEventById = new JTextField("");
-        //var completeEventByIdConstraints = new GridBagConstraints(3, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
-        completeEventById.setBounds(600,200, 200, 50);
+        //var completeEventByIdConstraints = new GridBagConstraints(3,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
+        completeEventById.setBounds(600,200,200,50);
+        completeEventById.setBackground(Color.lightGray);
         panel.add(completeEventById);
 
 
         JButton completeEvent = new JButton("Complete Event");
-        //var completeEventConstraints = new GridBagConstraints(6, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
+        //var completeEventConstraints = new GridBagConstraints(6,1,1,1,1,1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1,1,1,1), 0,0);
         completeEvent.setPreferredSize(new Dimension(250,100));
         Dimension completeEventSize = completeEvent.getPreferredSize();
-        completeEvent.setBounds(800,150, completeEventSize.width, completeEventSize.height);
+        completeEvent.setBounds(800,150, completeEventSize.width,completeEventSize.height);
         panel.add(completeEvent);
-        completeEvent.addActionListener(e -> {
+        completeEvent.addActionListener(e ->{
             String idToComplete = completeEventById.getText();
             int id = Integer.parseInt(idToComplete);
             try {
@@ -119,21 +122,23 @@ public class todoUI extends JFrame implements ActionListener {
         });
 
 
+
         JLabel deleteItemLabel = new JLabel("Enter Id of Item to Delete:");
-        deleteItemLabel.setBounds(600,250, 200,50);
+        deleteItemLabel.setBounds(600, 250, 200, 50);
         deleteItemLabel.setFont(new Font("Serif", Font.PLAIN, 18));
         //var deleteItemLabelConstraints = new GridBagConstraints(2, 3, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         panel.add(deleteItemLabel);
 
         JTextField deleteItemByIdinput = new JTextField("");
-        deleteItemByIdinput.setBounds(600,300,200,50);
+        deleteItemByIdinput.setBounds(600, 300, 200, 50);
+        deleteItemByIdinput.setBackground(Color.lightGray);
         //var deleteItemByIdConstraints = new GridBagConstraints(3, 3, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         panel.add(deleteItemByIdinput);
 
         JButton Cancel = new JButton("Delete Event");
-        Cancel.setPreferredSize(new Dimension(250,100));
+        Cancel.setPreferredSize(new Dimension(250, 100));
         Dimension CancelSize = Cancel.getPreferredSize();
-        Cancel.setBounds(800,250, CancelSize.width, CancelSize.height);
+        Cancel.setBounds(800, 250, CancelSize.width, CancelSize.height);
         //var CancelConstraints = new GridBagConstraints(6, 3, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0);
         panel.add(Cancel);
         Cancel.addActionListener(e -> {
@@ -148,13 +153,13 @@ public class todoUI extends JFrame implements ActionListener {
         JLabel refreshLabel = new JLabel();
         refreshLabel.setText("<HTML>Added something? Click here to refresh it!<HTML>");
         refreshLabel.setFont(new Font("Serif", Font.PLAIN, 18));
-        refreshLabel.setBounds(600,350,200,100);
+        refreshLabel.setBounds(600, 350, 200, 100);
         panel.add(refreshLabel);
 
         JButton refresh = new JButton("Refresh");
-        refresh.setPreferredSize(new Dimension(250,100));
+        refresh.setPreferredSize(new Dimension(250, 100));
         Dimension refreshSize = refresh.getPreferredSize();
-        refresh.setBounds(800, 350,refreshSize.width, refreshSize.height);
+        refresh.setBounds(800, 350, refreshSize.width, refreshSize.height);
         panel.add(refresh);
         refresh.addActionListener(e -> {
             try {
@@ -163,7 +168,6 @@ public class todoUI extends JFrame implements ActionListener {
                 ex.printStackTrace();
             }
         });
-
 
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
