1867485cb8c4d0640a64c5a29ac37a88b1dd2e9e
Jacob Deffendall <jddeffendall@bsu.edu>
2020-04-21 20:39:37 -0400

    Cleaned code and updated Meetings.md

--- src/main/java/offlineUI.java
+++ src/main/java/offlineUI.java
@@ -22,10 +22,8 @@ public class offlineUI extends JFrame {
 
         JPanel panel = new JPanel();
         setContentPane(panel);
-        //  JFrame frame = new JFrame();
         panel.setLayout(null);
         panel.setPreferredSize(new Dimension(1050, 550));
-        // frame.add(panel);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM dd yyyy HH:mm");
         var todoManager = new DatabaseUtils();
@@ -266,7 +264,5 @@ public class offlineUI extends JFrame {
         setVisible(true);
 
         todoManager.disposeResources();
-
     }
-
 }
